8283b2147e4126acf83c12126d658e55a9698925
IvanovichRU <KevinIDJ18@gmail.com>
2021-01-01 11:33:59 -0600

    Escribiendo conversiones

--- telepy/conversiones.py
+++ telepy/conversiones.py
@@ -7,26 +7,29 @@ def clasificar_actualización(objeto: dict):
         print('Error al convertir JSON.\nSe recibió:\n', objeto, '\nCerrando el programa.')
 
 def desempacar_mensaje(mensaje: dict):
-    mensaje_id = mensaje['message_id']
     if 'forward_from' in mensaje:
-        nuevo_mensaje = mensajes.MensajeReenviado()
-        nuevo_mensaje.id = mensaje_id
-        nuevo_mensaje.fecha = mensaje['date']
-        nuevo_mensaje.remitente = desempacar_usuario(mensaje['from'])
-        nuevo_mensaje.chat = desempacar_chat(mensaje['chat'])
-        if 'edit_date' in mensaje:
-            nuevo_mensaje.fecha_editado = mensaje['edit_date']
-        else:
-            nuevo_mensaje.fecha_editado = None
-        nuevo_mensaje.texto = mensaje['text']
-        if 'entities' in mensaje:
-            entidades = []
-            for entidad in mensaje['entities']:
-                entidades.append(desempacar_entidad(entidad))
-            nuevo_mensaje.entidades = entidades
-        else:
-            nuevo_mensaje.entidades = []
-        nuevo_mensaje.reenviado_remitente = mensaje['forward_from']
-        nuevo_mensaje.reenviado_de_chat = desempacar_chat(mensaje['forward_from_chat'])
-        nuevo_mensaje.reenviado_id = mensaje['forward_from_message_id']
-        nuevo_mensaje.reenviado_firma = mensaje['forward_signature']
+        desempacar_mensajereenviado(mensaje)
+
+def desempacar_mensajereenviado(mensaje: dict):
+    nuevo_mensaje = mensajes.MensajeReenviado()
+    nuevo_mensaje.id = mensaje_id
+    nuevo_mensaje.fecha = mensaje['date']
+    nuevo_mensaje.remitente = desempacar_usuario(mensaje['from'])
+    nuevo_mensaje.chat = desempacar_chat(mensaje['chat'])
+    if 'edit_date' in mensaje:
+        nuevo_mensaje.fecha_editado = mensaje['edit_date']
+    else:
+        nuevo_mensaje.fecha_editado = None
+    nuevo_mensaje.texto = mensaje['text']
+    if 'entities' in mensaje:
+        entidades = []
+        for entidad in mensaje['entities']:
+            entidades.append(desempacar_entidad(entidad))
+        nuevo_mensaje.entidades = entidades
+    else:
+        nuevo_mensaje.entidades = []
+    nuevo_mensaje.reenviado_remitente = mensaje['forward_from']
+    nuevo_mensaje.reenviado_de_chat = desempacar_chat(mensaje['forward_from_chat'])
+    nuevo_mensaje.reenviado_id = mensaje['forward_from_message_id']
+    nuevo_mensaje.reenviado_firma = mensaje['forward_signature']
+    nuevo_mensaje.reenviado_nombre = mensaje['']
