cd9aa51f3aaf4304682adcfec025950078c3552a
Kevin D <KevinIDJ18@gmail.com>
2021-08-31 04:41:40 -0500

    Cambios?

--- docs/conf.py
+++ docs/conf.py
@@ -52,7 +52,7 @@ language = 'es'
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 highlight_language = 'python3'
-pygments_style = 'rainbow_dash'
+pygments_style = 'sphinx'
 
 # -- Options for HTML output -------------------------------------------------
 
